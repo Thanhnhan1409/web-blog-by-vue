05f58f89c76a786653569ae5493a128de9ee27be
Thanhnhan1409 <phanthithanhnhan1409@gmail.com>
2024-02-11 13:20:39 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div class="main-content">
+    <h1>Happy New year!</h1>
+    <h2>Cầu chúc một năm mới tràn đầy sức khỏe, gia đình hạnh phúc hòa thuận, mọi việc điều như ý. Công việc và học tập đều rộng mở và đi lên. Chúc mừng năm mới!!</h2>
+  </div>
+</template>
+
+<style scoped>
+.main-content {
+  color: red;
+}
+</style>
