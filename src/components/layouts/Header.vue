5bf84e78ba1891c3b91d1b1ffdf7eb1c70b24cc1
Thanhnhan1409 <phanthithanhnhan1409@gmail.com>
2024-02-26 23:38:33 +0700

    feat: my posts pge and my favorites pages

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="header-container">
         ehehehhe | 
-        <el-avatar
-            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-        />
+        <router-link to="/information/1">
+            <el-avatar
+                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+            />
+        </router-link>
     </div>
 </template>
 <style scoped>
